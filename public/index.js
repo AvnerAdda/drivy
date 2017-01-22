4b1e381d7ba3bd639abaa155aa0bf535dfdcaaa9
Avner ADDA <addavner@gmail.com>
2017-01-22 15:25:44 +0100

    AllExercice - Avner ADDA

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ function getDate(id)
 {
 	// retal date 
 	var rtl = getRental(id);
-	var returnDate = new Date (rtl.returnDate);
+	var returnDate = new Date(rtl.returnDate);
 	var pickupDate = new Date(rtl.pickupDate);
 	//convert into millisecond
 	var time = 1+ (returnDate - pickupDate )/(24*3600*1000) ;
@@ -27,15 +27,14 @@ function rtl_time(time,price)
 {
 	//rental time = time x price
 	var rtltimer=time * price;
-	//console.log('Rental time ' + rentaltimer);
 	return rtltimer;
 }
 
-function rtl_distance(distance, price){
+function rtl_distance(distance, price)
+{
 	//rental distance = distance x price  
-	var rtldistancer=distance*price;
-	//console.log('Rental distance ' + rentaldistancer);
-	return rtldistancer;
+	var rtldistance=distance*price;
+	return rtldistance;
 }
 
 //Exercice 1 : Euro-Kilometers 
@@ -43,14 +42,13 @@ function new_rtl_Price()
 {
 	for(var i=0; i<rentals.length;i++)
 	{
-			
 		if(rentals[i].carId==cars[i].id)	
 		{	
 			var time = getDate(rentals[i].id);		
-		
+			//time component: the number of rental days multiplied by the car's price per day
 			var nrtl_time = rtl_time(time,cars[i].pricePerDay);
 			var nrtl_distance=rtl_distance(rentals[i].distance, cars[i].pricePerKm);
-			
+			//distance component: the number of kilometers multiplied by the car's price per km
 			//rental price = time + distance
 			var rtl_price=nrtl_time + nrtl_distance;
 			//Value change
@@ -59,60 +57,38 @@ function new_rtl_Price()
 	}
 }
 
-/*
-function PricePerConductor(rentals)
+function new_rtl_Price_prompt()
 {
-	var j = 0
-	while(j<rentals.length)
+	for(var i=0; i<rentals.length;i++)
 	{
-		console.log(j);
-		var conducerFirstName = rentals[j].driver.firstName;
-		console.log(conducerFirstName);
-		var beginDate = convertDate(rentals[j].pickupDate);
-		console.log(beginDate);
-		var returnDate = convertDate(rentals[j].returnDate);
-		console.log(returnDate);
-		var numberOfDay = getDays(beginDate,returnDate);
-		console.log(numberOfDay);
-		var carId = rentals[j].carId;
-		console.log(carId);
-		var priceD = pricePerDay(carId);
-		console.log(priceD);
-		var priceK = pricePerKm(carId);
-		console.log(priceK);
-		var distance = rentals[j].distance;
-		console.log(distance);
+	console.log(rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' ' +
+	 '\nCar: ' + rentals[i].carId +
+	 '\nDistance: '+rentals[i].distance+
+	 '\nRental Price : ' + rtl_time(getDate(rentals[i].id),cars[i].pricePerDay) + ' + ' + rtl_distance(rentals[i].distance, cars[i].pricePerKm) + ' = '+ rentals[i].price + ' ' + 'euros');
 	}
-}
-*/
-
-var rtlCommission = function rtlCommission(price,days)
-{
- 	//console.log(cars)
- 	console.log(rentals);
- 	PricePerConductor(rentals);
- 	console.log(rentals);
 }
 
 //Exercice 2 - Drive more, pay less
 
 function dsct_rentalPrice()
 {
-		
 	for(var i=0; i<rentals.length;i++){ 
 		var time_day = getDate(rentals[i].id);		
 		var rental_dsct;
+		//price per day decreases by 10% after 1 day
 		if(time_day==1)
 		{
 			rental_dsct = rentals[i].price;
 			console.log(rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' ' + '\nRental Price (no discount) : '  + rental_dsct  + ' euros');
 		}
+		//price per day decreases by 30% after 4 days
 		else if(time_day>1 && time_day<=4) 
 		{ 
 			rental_dsct = rentals[i].price*0.90; 
 			console.log(rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' ' + '\nRental Price Discount (-10%) : ' + rental_dsct + ' euros'); 
 			rentals[i].price= rental_dsct;
 		} 
+		//price per day decreases by 50% after 10 days
 		else if(time_day>4 && time_day<=10) {
 	
 			rental_dsct =rentals[i].price*0.70; 
@@ -124,7 +100,6 @@ function dsct_rentalPrice()
 			console.log(rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' ' + '\nRental Price Discount(-50%) : ' + rental_dsct + ' euros');
 			rentals[i].price= rental_dsct;
 		} 
-	
 	}
 }
 
@@ -135,23 +110,22 @@ function comm()
 	for(var i=0; i<rentals.length;i++)
 	{
 		var time_day = getDate(rentals[i].id);	
-	
+		//Drivy take a 30% commission on the rental price to cover their costs.
 		var comm = rentals[i].price * 0.70;
 		console.log('Commission: ' + rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' course is ' + comm + '.');
-	
+		//insurance: half of commission
 		var insur = comm / 2;
 		console.log('Insurrance: ' + rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' course is ' + comm + '.');
 		rentals[i].commission.insurance = insur;
-	
+		//roadside assistance 1€ per day
 		var roadAssist = time_day * 1;
 		console.log('Road assistance: ' + rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' course is ' + roadAssist + '.');
 		rentals[i].commission.assistance = roadAssist;
-	
+		//drivy: the rest
 		var total =comm - insur - roadAssist;
 		console.log('Drivy: ' + rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' course is ' + total + '.');
 		rentals[i].commission.drivy = total;
-	}
-		
+	}		
 }
 
 // Exercice 4 - The famous deductible
@@ -160,16 +134,20 @@ function opt_deduct()
 {
 	for(var i=0; i<rentals.length;i++)
 	{
-		var time_day = getDate(rentals[i].id);			
+		//In case of an accident, drivy applies a 800€ euros deductible.
+		//The driver can reduce the deductible amount from 800€ to 150€ with a deductible option for a few more euros per day.
+		var time_day = getDate(rentals[i].id);	
+		//The driver is charged an additional 4€/day when he chooses the deductible reduction option.		
 		var optDeduct=4*time_day;
 		var rtl_price_deduitopt;
-	
+		//If deductible reduction 
 		if(rentals[i].options.deductibleReduction==true)
 		{
 			rtl_price_deduitopt = rentals[i].price + optDeduct;
 			console.log(rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + '\nRental price (deduit option) : ' + rtl_price_deduitopt);
 			rentals[i].price=rtl_price_deduitopt;
 		}
+		//else 
 		else
 		{
 			rtl_price_deduitopt=rentals[i].price;
@@ -193,50 +171,57 @@ function payactors()
 	
 		for ( var j = 0 ; j < rentals.length ; j++)
 		{
-			if ( actors[i].rentalId == rentals[j].id ) 
+			if (actors[i].rentalId == rentals[j].id ) 
 			{
 				p = rentals[j].price;
 				var comm = rentals[j].price * 0.70;
 				var insur = comm / 2;
 				var roadAssist = day * 1;
 				var drivy = comm - insur - roadAssist;	
-			
-				for ( var k = 0 ; k < actors[i].payment.length; k++)
+				
+				for (var k = 0 ; k < actors[i].payment.length; k++)
 				{
 					switch(actors[i].payment[k].who)
 					{
 						case 'driver' : 
+						//the driver must pay the rental price and the (optional) deductible reduction
 						actors[i].payment[k].amount=p;
-						console.log(actors[i].payment[k].type + ' Driver Amount : ' + actors[i].payment[k].amount);
+						console.log(' Driver Amount : ' + actors[i].payment[k].amount);
 						break;
-				
+
 						case 'owner' :
+						//the owner receives the rental price minus the commission
 						actors[i].payment[k].amount=p-comm;
-						console.log(actors[i].payment[k].type +' Owner Amount : ' + actors[i].payment[k].amount);
+						console.log(' Owner Amount : ' + actors[i].payment[k].amount);
 						break;
 				
 						case 'insurance' :
+						//the insurance receives its part of the commission
 						actors[i].payment[k].amount=insur;
-						console.log(actors[i].payment[k].type +' Insurance Amount : ' + actors[i].payment[k].amount);
+						console.log(' Insurance Amount : ' + actors[i].payment[k].amount);
 						break;
 				
 						case 'assistance' :
+						//the assistance receives its part of the commission
 						actors[i].payment[k].amount=roadAssist;
-						console.log(actors[i].payment[k].type +' Assistance Amount : ' + actors[i].payment[k].amount);
+						console.log(' Assistance Amount : ' + actors[i].payment[k].amount);
 						break;
 				
 						case 'drivy' :
+						//drivy receives its part of the commission, plus the deductible reduction
+						//If reduction 
 						if(rentals[i].options.deductibleReduction==true)
 						{
 							var deduct=4*day;
 							actors[i].payment[k].amount=drivy + deduct;
-							console.log(actors[i].payment[k].type +' Drivy Amount : ' + actors[i].payment[k].amount);
+							console.log(' Drivy Amount : ' + actors[i].payment[k].amount);
  							break;
  						}
+ 						//else
  	 					else {
  							var deduct=0;
  							actors[i].payment[k].amount=drivy + deduct;
- 							console.log(actors[i].payment[k].lastName + ' ' + actors[i].firstName +'Drivy Amount : ' + actors[i].payment[k].amount);
+ 							console.log(' Drivy Amount : ' + actors[i].payment[k].amount);
  						break;
  						}
  					}			
@@ -252,9 +237,14 @@ function updat_modif()
 {	
  	for(var i=0; i<rentalModifications.length; i++)
  	{
- 		
  		for(var j=0; j<rentals.length; j++)
  		{		
+ 			//Drivy computes the amount that was already paid by (or given to) each actor
+ 			//Using the new dates/distance, drivy re-computes the price of the rental, the "deductible reduction" amount, the commission, ...
+			//Drivy compute the delta amount that must be debited/credited for each actor
+			//Each actor receives a debit/credit payment to settle his debt
+			//What is the delta amounts for each actor?
+
  			if(rentalModifications[i].rentalId == rentals[j].id)
  			{	
  				if( typeof rentalModifications[i].returnDate != "undefined")
@@ -281,11 +271,10 @@ function updat_modif()
  		}
 	}
 }
-
-
  
 function apply_modif()
 {
+	//All new data
  	console.log('Modifications');
 	new_rtl_Price();
 	dsct_rentalPrice();
@@ -458,37 +447,51 @@ var rentalModifications = [{
   'pickupDate': '2015-12-05'
 }];
 
+console.group('DATA:')
 console.time()
-//console.log(cars);
+console.log('Cars \n');
+console.log(cars); 
+console.log('Rentals \n');
 console.log(rentals);
-//console.log(actors);
+console.log('Actors \n');
+console.log(actors);
+console.log('Rental Modifications \n');
 console.log(rentalModifications);
+console.groupEnd();
+
 console.group('EXERCICE 1 - Euro-Kilometers')
 new_rtl_Price()
-for(var i=0; i<rentals.length;i++)
-{
-	console.log(rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName + ' ' +
-	 '\nCar: ' + rentals[i].carId +
-	 '\nDistance: '+rentals[i].distance+
-	 '\nRental Price : ' + rtl_time(getDate(rentals[i].id),cars[i].pricePerDay) + ' + ' + rtl_distance(rentals[i].distance, cars[i].pricePerKm) + ' = '+ rentals[i].price + ' ' + 'euros');
-}
+new_rtl_Price_prompt()
 console.log('Price have change !')
 console.timeEnd()
+console.groupEnd()
+
 
 console.group('EXERCICE 2 - Drive more, pay less')
 dsct_rentalPrice();
 console.log('Rental Price have change !')
 console.timeEnd()
+console.groupEnd()
+
+
 console.group('EXERCICE 3 - Give me all your money')
 comm();
 console.timeEnd()
+console.groupEnd()
+
 console.group('EXERCICE 4 - The famous deductible')
 opt_deduct();
 console.timeEnd()
+console.groupEnd()
+
 console.group('EXERCICE 5 - Pay the actors')
 payactors();
 console.timeEnd()
+console.groupEnd()
+
 console.group('EXERCICE 6 - Rental modification')
-payactors();
+apply_modif();
 console.timeEnd()
+console.groupEnd()
+
 console.log("FINISH!")
